5368aa05d6846e1717dd2f64070f80fc2bad5ced
Agnan Fernandes <agnanfernandes@gmail.com>
2023-09-13 15:54:57 -0400

    feat: added tables, db and templates

---
+++ posts.py
@@ -0,0 +1,18 @@
+posts = [
+    { 
+        "titulo": "Minha primeira postagem",
+        "texto": """Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Proin dapibus sed urna nec tristique. Nam porta, mauris vel eleifend 
+        feugiat, nisi arcu venenatis metus, ac luctus eros enim nec felis. 
+        Integer diam eros, porttitor eget ultricies a, fermentum ac augue. 
+        Phasellus vitae euismod justo. Suspendisse sit amet mauris et metus dictum sodales sed 
+        ac risus. Nulla ultricies porttitor quam, at sodales orci viverra quis. Ut sodales 
+        lorem at justo interdum, nec eleifend elit pharetra. Nulla facilisi. Aliquam elit dui, 
+        convallis non lobortis faucibus, porttitor in justo. Praesent id ornare odio."""
+    },
+    {
+        "titulo": "Segundo Post",
+        "text": "teste"
+    }
+]
+
